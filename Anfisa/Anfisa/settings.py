1843486179e6b9b08e86e5a198a771183949f126
Дмитрий Емельянов <thl00@rambler.ru>
2022-09-19 14:15:16 +0300

    создание приложения posts

--- Anfisa/Anfisa/settings.py
+++ Anfisa/Anfisa/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'posts.apps.PostsConfig'
     'ice_cream.apps.IceCreamConfig',
     'django.contrib.admin',
     'django.contrib.auth',
